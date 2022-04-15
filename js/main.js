a5d65b9807c2155d629fef73a4a82cee5a2ce663
Gianluca <gianlucafava.17@gmail.com>
2022-04-15 18:44:36 -0300

    Valide que el usuario no ingrse letras en vez de numeros

--- js/main.js
+++ js/main.js
@@ -1,32 +1,34 @@
+let continua = true
 
 let saldo = 1000
-while (true) {
+while (continua) {
     alert(".:MENU:. \n 1. Ingresar dinero en la cuenta \n 2. Retirar dinero de la cuenta \n 3. Mostrar dinero disponible \n 4. Salir")
-    opcion = parseInt(prompt("Digite una opción: "))
+    opcion = parseFloat(prompt("Digite una opción: "))
 
-
-    if (opcion===1){
+    if (opcion === 1) {
         extra = parseFloat(prompt("Cuanto dinero desea ingresar en la cuenta ->"))
-        saldo += extra
-        alert("Su saldo es de:" + saldo)
-    }
-    else if (opcion===2){
-        retirar = parseFloat(prompt("Cuanto dinero desea retirar? -> "))
-        if (retirar>saldo)
-            alert("No tiene esa cantidad de dinero")
-        else
-            (saldo-=retirar)
-            alert("Dinero en la cuenta: " + saldo)
-    }
-    else if (opcion===3){
-        alert("Dinero en la cuenta: " + saldo)
-    }
-    else if (opcion===4){
-        alert("Gracias por usar su cajero automatico")
-        break
-    }
-    else{
-        alert("Error opcion inválida")
+        if (isNaN(extra)) {
+            alert("Por favor ingrese un número válido")
+        } else {
+            saldo += extra
+            alert("Su saldo es de:" + saldo)
         }
-
+    } else if (opcion === 2) {
+        retirar = parseFloat(prompt("Cuanto dinero desea retirar? -> "))
+        if (isNaN(retirar)) {
+            alert("Por favor ingrese un número válido")
+        } else if (retirar > saldo) {
+            alert("No tiene esa cantidad de dinero")
+        } else
+            (saldo -= retirar)
+        alert("Dinero en la cuenta: " + saldo)
+    } else if (opcion === 3) {
+        alert("Dinero en la cuenta: " + saldo)
+    } else if (opcion === 4) {
+        alert("Gracias por usar su cajero automatico")
+        continua = false
+    } else {
+        alert("Error opcion inválida")
     }
+
+}
